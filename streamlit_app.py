fc6e08487f3b1de64d7ba6d3a59d52d3e89e2ae6
ryootan <116160051+ryootan@users.noreply.github.com>
2022-10-20 05:43:29 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from pypfopt.expected_returns import mean_historical_return
+from pypfopt.risk_models import CovarianceShrinkage
+
 from pypfopt import EfficientFrontier
 from pypfopt import risk_models
 from pypfopt import expected_returns
