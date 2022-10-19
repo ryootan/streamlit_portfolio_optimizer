43b6c682dc508e45cca58fc07697ad43fb9e45a3
ryootan <116160051+ryootan@users.noreply.github.com>
2022-10-20 06:05:49 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from pypfopt.efficient_frontier import EfficientFrontier
 
 assets = ['A','B']
-mu = pd.Series([1.0,2.0],index=assets)
+mu = pd.Series([1.0,20.0],index=assets)
 S = pd.DataFrame({'A':[0.01,0.05],'B':[0.05,1.00]},index=assets)
 ef = EfficientFrontier(mu, S)
 ef.max_sharpe()
