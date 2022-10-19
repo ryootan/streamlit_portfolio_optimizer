c268e97fdab7504f9c72466ee8d4c124ab5b96f4
ryootan <116160051+ryootan@users.noreply.github.com>
2022-10-20 05:57:05 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ mu = pd.Series([1.0,2.0],index=assets)
 S = pd.DataFrame({'A':[0.01,0.05],'B':[0.05,1.00]},index=assets)
 ef = EfficientFrontier(mu, S)
 weights = ef.max_sharpe()
-
-
+print(weights)
+ef.portfolio_performance(verbose=True)
 
 x = 10
 'x: ', x 
