77a057422b874b32baab7634916574af1e5c8f62
ryootan <116160051+ryootan@users.noreply.github.com>
2022-10-21 06:25:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ if uploaded_file is not None:
   input_df['Upper Bound'] = 100.0
   input_df['Risk Weight'] = 0.0
   
-  st.write(input_df.to_csv(sep='\t')
+  st.write(input_df.to_csv(sep='\t'))
   
   input_txt = st.text_area('Insert optimization inputs')
     
